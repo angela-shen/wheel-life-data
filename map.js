6e7c486f8b3d6b57903715c2502a44f047c13b58
angela-shen <136782411+angela-shen@users.noreply.github.com>
2025-02-21 12:36:02 -0800

    part of step 2

---
+++ map.js
@@ -0,0 +1,31 @@
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoiYTlzaGVuIiwiYSI6ImNtN2Y2YXRmajBnNzIycXE3Y2M2N3Zra2cifQ.s18kxUj1URDC_9eyrgO_RQ';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/dark-v11', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18 // Maximum allowed zoom
+});
+
+map.on('load', () => { 
+  //code 
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+  });
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': '#32D400',  // A bright green using hex code
+      'line-width': 5,          // Thicker lines
+      'line-opacity': 0.6       // Slightly less transparent
+    }
+  });
+});
+  
